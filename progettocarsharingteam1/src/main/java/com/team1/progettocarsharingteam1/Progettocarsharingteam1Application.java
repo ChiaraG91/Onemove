3ef18ef14076cef473df7988b74258dcde757aae
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2024-03-02 15:35:21 +0100

    Add files via upload

---
+++ progettocarsharingteam1/src/main/java/com/team1/progettocarsharingteam1/Progettocarsharingteam1Application.java
@@ -0,0 +1,13 @@
+package com.team1.progettocarsharingteam1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Progettocarsharingteam1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Progettocarsharingteam1Application.class, args);
+	}
+
+}
---
+++ progettocarsharingteam1/src/test/java/com/team1/progettocarsharingteam1/Progettocarsharingteam1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team1.progettocarsharingteam1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Progettocarsharingteam1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
